712458b788ea891ff3e22d3d4e971938106bfb4c
MaryamS5 <45773@students.riphah.edu.pk>
2024-10-19 09:58:00 -0700

    First Commit

---
+++ exportfile/App.js
@@ -0,0 +1,6 @@
+//importfile.js
+import{ getFlowers}= required("./importfile.js");
+
+//calling info
+const flowers=getFlowers();
+console.log(flowers); 
---
+++ exportfile/comonents/importfile.js
@@ -0,0 +1,12 @@
+
+// Function that returns an array of flower objects
+function getFlowers() {
+  const flowers = [
+      { name: 'Rose', color: 'Red', fragrance: 'Strong', season: 'Summer' },
+      { name: 'Lily', color: 'White', fragrance: 'Mild', season: 'Spring' },
+    
+  ];
+  return flowers;
+}
+// Exporting the function using module.exports
+module.exports = getFlowers;
