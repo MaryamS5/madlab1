f0b802a8c94862892c955c2eb9d254b108869c7c
MaryamS5 <45773@students.riphah.edu.pk>
2024-10-20 07:56:53 -0700

    Create notfiction.jsx

---
+++ assig1/notfiction.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import  {View,Text,Image,ImageBackground,StyleSheet,Button, TextInpute} from "react-native";
+import {MatrialIcon} from 'react-native-vector-icons/Ionicons'; 
+const notifi=(nav)=>{
+
+<View >
+
+<Button style={{ color:"yellow", border-color: "solid black"}} Title="" go to home" onpress={()=>navigation.goback()}/>
+</View>
